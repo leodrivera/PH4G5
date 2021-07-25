f96852bdd1fa257d82fda29b18487638c0670a64
root <root@homeBrain>
2021-07-25 19:12:31 -0300

    Frontend revisado

--- viver-de-rir-django/backend/index/views.py
+++ viver-de-rir-django/backend/index/views.py
@@ -5,11 +5,13 @@ import requests
 # Create your views here.
 
 def index(request):
-    image_url = requests.get('http://localhost:8000/api/foto-principal/?format=json').json()[0]['image_url']
-    
+    image_url = requests.get('http://localhost:8000/api/foto-principal/?format=json').json()[0]
 
+    acoes = requests.get('http://localhost:8000/api/acoes/?format=json').json()
+    
     context = {
-        'image_url': image_url
+        'image_url': image_url,
+        'acoes': acoes,
     }
     
     return render(request, 'base.html', context)
